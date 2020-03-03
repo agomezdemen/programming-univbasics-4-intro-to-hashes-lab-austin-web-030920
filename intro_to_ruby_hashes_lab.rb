9b34e3334dd94d87d9b4057d8ec4cfb96665dc47
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 22:53:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,25 @@
 def new_hash
-  # return an empty hash
+  hashy = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hashy = {greeting: "hi"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hashy = {name: "Grace Hopper"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  hashy = {id: 2}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hashy = {key => value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
